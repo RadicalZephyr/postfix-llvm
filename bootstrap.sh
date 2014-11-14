5a37f8a077c8af78ae1600a45ef6435d96ce4850
Geoff Shannon <geoffpshannon@gmail.com>
2014-11-14 13:27:56 -0500

    Add the llvm deb package sources

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,30 @@
 #!/usr/bin/env bash
 
+cat >> /etc/apt/sources.list <<EOF
+deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty main
+deb-src http://llvm.org/apt/trusty/ llvm-toolchain-trusty main
+# 3.4
+deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.4 main
+deb-src http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.4 main
+# 3.5
+deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.5 main
+deb-src http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.5 main
+EOF
+
+wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | apt-key add -
+
 apt-get update
 
 # General development dependencies
 apt-get install -y cmake build-essential
 
 # Install llvm libraries
-apt-get install -y llvm llvm-dev llvm-runtime
+apt-get install -y libllvm-3.4-ocaml-dev libllvm3.4 libllvm3.4-dbg lldb-3.4 llvm-3.4 llvm-3.4-dev llvm-3.4-doc llvm-3.4-examples llvm-3.4-runtime  lldb-3.4-dev
 
 # Install clang compiler
-apt-get install -y clang
+apt-get install -y clang-3.4 clang-3.4-doc libclang-common-3.4-dev libclang-3.4-dev libclang1-3.4 libclang1-3.4-dbg
 
+apt-get install -y clang-modernize-3.4 clang-format-3.4 python-clang-3.4
 
 # Setup my personal configuration files
 apt-get install -y unzip
