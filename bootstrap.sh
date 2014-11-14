045e3dadc50c47f0e31c8b02a2a0fc91d7170fdd
Geoff Shannon <geoffpshannon@gmail.com>
2014-11-14 12:38:53 -0500

    Add installing the clang compiler

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 apt-get update
+
+# Install llvm libraries
 apt-get install -y llvm-3.5 llvm-3.5-dev llvm-3.5-doc llvm-3.5-examples llvm-3.5-runtime llvm-3.5-tools
+
+# Install clang compiler
+apt-get install -y clang-3.5 clang-3.5-doc clang-3.5-examples
