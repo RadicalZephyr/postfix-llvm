c16b16f2b4f408ff1b04527392ef2fa206f07dc9
Geoff Shannon <geoffpshannon@gmail.com>
2014-11-14 12:16:43 -0500

    Attempt to get CMake to link against homebrew llvm

--- postfix.cpp
+++ postfix.cpp
@@ -1,3 +1,27 @@
-int main() {
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include <fstream>
+#include <iostream>
+
+using namespace llvm;
+
+static cl::opt<std::string>
+InputProgram(cl::Positional, cl::desc("<postfix input>"));
+
+static cl::opt<std::string>
+OutputFilename("o", cl::desc("Output filename"), cl::value_desc("filename"));
+
+
+int main(int argc, char **argv) {
+    cl::ParseCommandLineOptions(argc, argv, " Postfix compiler\n");
+
+    if (InputProgram == "") {
+        errs() << "Error: You must provide a postfix program as input."
+            "Use --help to see the options.\n";
+        abort();
+    }
+
     return 0;
 }
