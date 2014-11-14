a81f8b295e812f95e32ae9d07afef32acabfb9ce
Geoff Shannon <geoffpshannon@gmail.com>
2014-11-14 12:41:45 -0500

    Add general dev dependencies

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,9 @@
 
 apt-get update
 
+# General development dependencies
+apt-get install -y cmake build-essential
+
 # Install llvm libraries
 apt-get install -y llvm-3.5 llvm-3.5-dev llvm-3.5-doc llvm-3.5-examples llvm-3.5-runtime llvm-3.5-tools
 
