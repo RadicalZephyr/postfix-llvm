9ff6177f37c92bd23aa374c7aca68498be91d646
Geoff Shannon <geoffpshannon@gmail.com>
2014-11-14 23:21:47 -0500

    Utilize the new sexp parser

--- postfix.cpp
+++ postfix.cpp
@@ -4,6 +4,9 @@
 
 #include <fstream>
 #include <iostream>
+#include <sstream>
+
+#include "sexp/sexp.h"
 
 using namespace llvm;
 
@@ -13,14 +16,24 @@ InputProgram(cl::Positional, cl::desc("<postfix input>"));
 static cl::opt<std::string>
 OutputFilename("o", cl::desc("Output filename"), cl::value_desc("filename"));
 
-
 int main(int argc, char **argv) {
     cl::ParseCommandLineOptions(argc, argv, " Postfix compiler\n");
 
     if (InputProgram == "") {
         errs() << "Error: You must provide a postfix program as input.\n"
             "Use --help to see the options.\n";
-        exit(1);
+        return 1;
+    }
+
+    std::stringstream postfixstream(InputProgram);
+
+    Sexpressionizer sexper(postfixstream);
+
+    try {
+        SexpPtr exprs = sexper.getNextSexp();
+    } catch (ParseExceptionPtr e) {
+        std::cerr << e->what() << std::endl;
+        return 1;
     }
 
     return 0;
